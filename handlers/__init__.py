70c121ef5c4eb2282777ea4fa6c058028e98eb87
ashishsingh9457 <ashishksingh9457@gmail.com>
2026-01-05 11:09:38 +0000

    fix: add missing terabox_handler import in handlers/__init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,6 @@
 from .start import start
 from .help import help_command
 from .utility import show_time, show_date
+from .terabox import terabox_handler
 
-__all__ = ["start", "help_command", "show_time", "show_date"]
+__all__ = ["start", "help_command", "show_time", "show_date", "terabox_handler"]
